3179f89aaf08eb19e4ef2c65b1646960fd9f7a9f
gtema <gtema@localhost.localdomain>
2017-02-09 14:58:09 +0100

    another try with exc handling for the init on heroku

--- init.py
+++ init.py
@@ -18,61 +18,70 @@ with app.app_context():
     db.create_all()
     app.logger.info('Filling DB with test data')
 
-    app.logger.info('Filling DB with categories')
-    with open('categories.csv') as f:
-        # assume first line is header
-        cf = csv.DictReader(f, delimiter=',')
-        for row in cf:
-            db.session.add(
-                StockProductCategory(
-                    id=row.get('Id'),
-                    parent_id=row.get('Parent_id'),
-                    name=row.get('Name'),
-                    prio=row.get('Prio')
-                )
-            )
-
-    app.logger.info('Filling DB with products')
-    with open('products.csv') as f:
-        # assume first line is header
-        cf = csv.DictReader(f, delimiter=',')
-        for row in cf:
-            db.session.add(
-                StockProduct(
-                    id=row.get('Id'),
-                    category_id=row.get('Category_id'),
-                    name=row.get('Name'),
-                    volume=row.get('Volume'),
-                    sum_amounts=row.get('Sum_amounts')
-                )
-            )
-
     # Create the default roles
     app.logger.info('Filling DB with users')
-    basic = user_datastore.find_or_create_role(name='User', description="Basic user")
-    admin = user_datastore.find_or_create_role(name='Admin', description='API Administrator')
+    try:
+        basic = user_datastore.find_or_create_role(name='User', description="Basic user")
+        admin = user_datastore.find_or_create_role(name='Admin', description='API Administrator')
 
-    # Create the default users
-    user_datastore.create_user(username='papa', email='test1@gmail.com', password=encrypt_password('apap'),
-        first_name="Papa", last_name="Family",
-        api_key=b64encode(os.urandom(48)).decode('ascii'))
-    user_datastore.create_user(username='mama', email='test2@gmail.com', password=encrypt_password('amam'),
-            first_name="Mama", last_name="Family",
+        # Create the default users
+        user_datastore.create_user(username='papa', email='test1@gmail.com', password=encrypt_password('apap'),
+            first_name="Papa", last_name="Family",
             api_key=b64encode(os.urandom(48)).decode('ascii'))
-    user_datastore.create_user(username='child', email='test3@gmail.com', password=encrypt_password('dlihc'),
-            first_name="Child", last_name="Family")
+        user_datastore.create_user(username='mama', email='test2@gmail.com', password=encrypt_password('amam'),
+                first_name="Mama", last_name="Family",
+                api_key=b64encode(os.urandom(48)).decode('ascii'))
+        user_datastore.create_user(username='child', email='test3@gmail.com', password=encrypt_password('dlihc'),
+                first_name="Child", last_name="Family")
 
-    papa = user_datastore.find_user(username='papa')
-    mama = user_datastore.find_user(username='mama')
-    child = user_datastore.find_user(username='child')
+        papa = user_datastore.find_user(username='papa')
+        mama = user_datastore.find_user(username='mama')
+        child = user_datastore.find_user(username='child')
 
-    user_datastore.activate_user(papa)
-    user_datastore.activate_user(mama)
-    user_datastore.activate_user(child)
+        user_datastore.activate_user(papa)
+        user_datastore.activate_user(mama)
+        user_datastore.activate_user(child)
 
-    user_datastore.add_role_to_user(papa, admin)
-    user_datastore.add_role_to_user(mama, admin)
-    user_datastore.add_role_to_user(child, basic)
+        user_datastore.add_role_to_user(papa, admin)
+        user_datastore.add_role_to_user(mama, admin)
+        user_datastore.add_role_to_user(child, basic)
 
-    # Save users
-    db.session.commit()
+        # Save users
+        db.session.commit()
+    except:
+        app.logger.exception('Exception occured during installing users', sys.exc_info()[0])
+        raise
+
+    try:
+        app.logger.info('Filling DB with categories')
+        with open('categories.csv') as f:
+            # assume first line is header
+            cf = csv.DictReader(f, delimiter=',')
+            for row in cf:
+                db.session.add(
+                    StockProductCategory(
+                        id=row.get('Id'),
+                        parent_id=row.get('Parent_id'),
+                        name=row.get('Name'),
+                        prio=row.get('Prio')
+                    )
+                )
+
+        app.logger.info('Filling DB with products')
+        with open('products.csv') as f:
+            # assume first line is header
+            cf = csv.DictReader(f, delimiter=',')
+            for row in cf:
+                db.session.add(
+                    StockProduct(
+                        id=row.get('Id'),
+                        category_id=row.get('Category_id'),
+                        name=row.get('Name'),
+                        volume=row.get('Volume'),
+                        sum_amounts=row.get('Sum_amounts')
+                    )
+                )
+        db.session.commit()
+    except:
+        app.logger.exception('Exception occured during installing data', sys.exc_info()[0])
+        raise
